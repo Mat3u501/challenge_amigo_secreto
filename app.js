28893cdce49b87f72124f549a58d78e1685f98ab
Mat3u501 <90454384+Mat3u501@users.noreply.github.com>
2025-03-17 22:32:33 -0500

    Se creó la lista y la función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaAmigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    listaAmigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
